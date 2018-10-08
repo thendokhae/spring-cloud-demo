9fa9a4426a0f20a4b2be2b6dc0617b38c4c5e463
B2B\MakahaneT <thendo.makahane@reedbusiness.com>
2018-10-08 10:38:37 +0200

    currency exchange service added

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1001,'USD','ZAR','14.78', 0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1002,'EUR','ZAR','17.12',0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1003,'AUSD','ZAR','9.95',0);
+
